7e76e2f38232d474c9fb7dc7d352c610f2c8f619
AlexandrYangirov <153558017+AlexandrYangirov@users.noreply.github.com>
2024-01-11 22:13:27 +0300

    hw-2.jsversion1

---
+++ hw-2.js
@@ -0,0 +1,71 @@
+//Задание 1
+let meaning = 10
+//age = promt(a)
+alert(meaning);
+let twenty = 20
+alert(twenty);
+
+//Задание 2
+let iphoneRealiseYear = "год выпуска первого iPhone 2007"
+alert(iphoneRealiseYear);
+//Задание 3
+let jsCreator = "создатель языка JavaScript Брендан Эйх"
+alert(jsCreator);
+
+//Задание 4
+let num1 = 10, num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
+
+//Задание 5
+let result = 2**5;
+alert(result);
+
+//Задание 6
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
+
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+let age = "prompt"("сколько вам лет?");
+alert (age);
+ 
+//Задание 9
+let user = {
+    name: "john",
+    age: 30,
+    isAdmin: true
+};
+
+//Задание 9.1
+user["citi of residence"] = "New York";
+
+//Задание 9.2
+user.age = 35;
+
+//Задание 9.3
+delete user ["citi of residence"];
+
+//Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+
+//Задание 10
+let userNeme = prompt("Как вас зовут?");
+alert(`Привет,${userNeme}!`);
+
+
+
