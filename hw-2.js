6f31b8542b4a55fe84bd9e3f192ffbd239a47a90
AlexandrYangirov <153558017+AlexandrYangirov@users.noreply.github.com>
2024-01-12 14:15:26 +0300

    new version 12/01/2024

--- hw-2.js
+++ hw-2.js
@@ -6,30 +6,6 @@ let twenty = 20
 alert(twenty);
 
 //Задание 2
-<<<<<<< HEAD
-let iphoneRealiseYear = "год выпуска первого iPhone 2007"
-alert(iphoneRealiseYear);
-//Задание 3
-let jsCreator = "создатель языка JavaScript Брендан Эйх"
-alert(jsCreator);
-
-//Задание 4
-let num1 = 10, num2 = 2;
-alert(num1 + num2);
-alert(num1 - num2);
-alert(num1 * num2);
-alert(num1 / num2);
-
-//Задание 5
-let result = 2**5;
-alert(result);
-
-//Задание 6
-let a = 9;
-let b = 2;
-let remainder = a % b;
-alert(remainder);
-=======
 const iphonereliaseyear = "год выпуска первого iPhone 2007"
 alert(iphonereliaseyear);
 //Задание 3
@@ -61,7 +37,6 @@ b = 2;
 
 let remainder = a % b;
 alert("Остаток от деления a на b: " + remainder);
->>>>>>> d8d5a028c602f3d8d79751297259fc847863dc4a
 
 //Задание 7
 let num = 1;
@@ -74,7 +49,6 @@ num--;
 alert(num);
 
 //Задание 8
-<<<<<<< HEAD
 let age = "prompt"("сколько вам лет?");
 alert (age);
  
@@ -104,36 +78,3 @@ alert(`Привет,${userNeme}!`);
 
 
 
-=======
-let age = prompt("Сколько вам лет?");
-alert(age);
-
-//Задание 9
-const user = {
-    name: "Alexandr",
-    age: 33,
-    isadmin: true
-};
-
-//Задание 9.1
-//способ 1
-javascript
-user.cityOfResidence = "Moscow";
-//способ 2
-javascript
-user["city of residence"] = "Moscow";
-
-//Задание 9.2
-user.age = 25;
-
-//Задание 9.3
-delete user.city_of_residence;
-
-//Задание 9.4
-let info = prompt("Какую информацию хотите узнать о пользователе?")
-alert(user[info])
-
-//Задание 10
-let userName = prompt("Как вас зовут?");
-alert(`Привет,$(userName)!`);
->>>>>>> d8d5a028c602f3d8d79751297259fc847863dc4a
